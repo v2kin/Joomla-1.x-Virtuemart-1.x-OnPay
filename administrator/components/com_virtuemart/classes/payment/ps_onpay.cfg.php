7de024eb42f63adf8f27b561abcc407e163d0b06
v2kin <admin@k27.ru>
2013-11-24 17:44:37 +0400

    test

---
+++ administrator/components/com_virtuemart/classes/payment/ps_onpay.cfg.php
@@ -0,0 +1,8 @@
+<?php if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die('Direct Access to this location is not allowed.');
+
+define ('ONPAY_LOGIN', 'demo');
+define ('ONPAY_SECRET_KEY', 'a2su3ig8y');
+define ('ONPAY_NOTIFY_EMAIL', 'vs@onpay.ru');
+define ('ONPAY_PENDING_STATUS', 'P');
+define ('ONPAY_VERIFIED_STATUS', 'R');
+?>
